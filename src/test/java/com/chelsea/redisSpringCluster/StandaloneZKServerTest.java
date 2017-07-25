5d083f01764c380708b963bed899d9ffc911a900
412745310@qq.com <412745310@qq.com>
2017-07-25 16:33:05 +0800

    redis集群

---
+++ src/test/java/com/chelsea/redisSpringCluster/StandaloneZKServerTest.java
@@ -0,0 +1,27 @@
+package com.chelsea.redisSpringCluster;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import redis.clients.jedis.JedisCluster;
+
+public class StandaloneZKServerTest{
+	
+	ApplicationContext ctx = null;
+	JedisCluster jedis = null;
+	
+	@Before
+	public void init(){
+		ctx = new ClassPathXmlApplicationContext("spring.xml");
+		jedis = (JedisCluster)ctx.getBean("jedisCluster");
+	}
+	
+	@Test
+	public void testJedis(){
+		String value = jedis.get("testkey");
+		System.out.println(value);
+	}
+	
+}
